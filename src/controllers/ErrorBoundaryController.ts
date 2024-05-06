d0d45903a32eb7e0300cea604aa8868e1e7b8c96
Voguh <voguhofc@protonmail.com>
2024-05-05 22:20:01 -0300

    fix: error bondary catch when is unknown error

--- src/controllers/ErrorBoundaryController.ts
+++ src/controllers/ErrorBoundaryController.ts
@@ -7,11 +7,14 @@ export default class ErrorBoundaryController {
   public static async catch(err: any, _req: Request, res: Response, _next: NextFunction): Promise<void> {
     if (err instanceof BaseHTTPError) {
       res.status(err.statusCode).send(err.message)
-    } else if (err instanceof Error) {
-      Logger.error(err.message, err)
     } else {
-      Logger.error(err)
+      if (err instanceof Error) {
+        Logger.error(err.message, err)
+      } else {
+        Logger.error(err)
+      }
+
+      res.status(500).send('An unknown error occurred')
     }
-    res.status(500).send('An unknown error occurred')
   }
 }
