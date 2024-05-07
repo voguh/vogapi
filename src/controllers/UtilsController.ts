c5d006b5675cf9d0cfe7c4a98e72412d0027902b
Voguh <voguhofc@protonmail.com>
2024-05-07 20:44:51 -0300

    feat: include status endpoint and refactor title in index.html

--- src/controllers/UtilsController.ts
+++ src/controllers/UtilsController.ts
@@ -18,8 +18,12 @@ export default class UtilsController extends RestControler {
   @GET('/')
   @SwaggerPath({ summary: 'Returns a HTML with home page' })
   public async getIndex(req: Request, res: Response): Promise<void> {
+    const pkgJson = JSON.parse(fs.readFileSync(path.resolve(ROOT_PATH, 'package.json'), 'utf-8'))
+
     const filePath = path.resolve(PUBLIC_PATH, 'index.html')
-    const rawContent = this._getFileData(res, filePath)
+    let rawContent = this._getFileData(res, filePath)
+    rawContent = rawContent.replaceAll('{{displayName}}', pkgJson.displayName)
+    rawContent = rawContent.replaceAll('{{version}}', pkgJson.version)
     res.send(rawContent)
   }
 
@@ -41,6 +45,16 @@ export default class UtilsController extends RestControler {
     let rawContent = this._getFileData(res, filePath)
     rawContent = rawContent.replaceAll('{{LICENSE_NAME}}', pkgJson.license)
     rawContent = rawContent.replaceAll('{{LICENSE_BODY}}', licenseBody)
+    res.send(rawContent)
+  }
+
+  @GET('/status')
+  @SwaggerPath({ summary: 'Returns a text with system version' })
+  public async status(req: Request, res: Response): Promise<void> {
+    const pkgJson = JSON.parse(fs.readFileSync(path.resolve(ROOT_PATH, 'package.json'), 'utf-8'))
+    const rawContent = `VogAPI v${pkgJson.version}`
+    res.setHeader('Content-Type', 'text/plain')
+    res.setHeader('Content-Length', rawContent.length)
     res.send(rawContent)
   }
 }
